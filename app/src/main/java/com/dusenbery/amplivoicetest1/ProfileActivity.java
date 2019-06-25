5beec1bde9ef4e9d504bef25179cf3b10914ff0c
Karl Dusenbery <pug33957@sbcglobal.net>
2019-06-25 22:40:22 +0800

    Figured out how to make a circular image that has a square image cropped inside it

--- app/src/main/java/com/dusenbery/amplivoicetest1/ProfileActivity.java
+++ app/src/main/java/com/dusenbery/amplivoicetest1/ProfileActivity.java
@@ -1,8 +1,10 @@
 package com.dusenbery.amplivoicetest1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +12,31 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        // Enables the back button in the action bar at the top of the screen
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Removes the app title in the action bar at the top of the screen
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        ImageView imageView = (ImageView) findViewById(R.id.profile_image);
+        imageView.setImageDrawable(getResources().getDrawable(R.drawable.profile));
+    }
+
+    // Creates a listener for the action bar at the top of the screen
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // Enables the action bar at the top of the screen
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
     }
 }
