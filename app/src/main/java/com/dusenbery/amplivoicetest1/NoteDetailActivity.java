b5e3f34d5a65d3250e57a4025993b94a65f107cc
Karl Dusenbery <pug33957@sbcglobal.net>
2019-07-09 19:47:35 +0800

    Fixed action bar on top of NoteDetailActivity to have a back button and no app title.

--- app/src/main/java/com/dusenbery/amplivoicetest1/NoteDetailActivity.java
+++ app/src/main/java/com/dusenbery/amplivoicetest1/NoteDetailActivity.java
@@ -3,6 +3,8 @@ package com.dusenbery.amplivoicetest1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class NoteDetailActivity extends AppCompatActivity {
 
@@ -10,5 +12,28 @@ public class NoteDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_detail);
+
+        // Enables the back button in the action bar at the top of the screen
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Removes the app title in the action bar at the top of the screen
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+    // Creates a listener for the action bar at the top of the screen
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // Enables the action bar at the top of the screen
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
     }
 }
